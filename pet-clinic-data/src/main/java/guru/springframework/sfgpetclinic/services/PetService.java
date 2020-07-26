018f5c9287d6729a680736dfcaaf581eaf65fab3
msharma <msharma@paypal.com>
2020-07-26 14:58:23 -0700

    Adding common interface. closes #11

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -3,12 +3,6 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.models.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long Id);
-    Set<Pet> findAll();
-    Pet save(Pet pet);
-
+public interface PetService extends CrudService<Pet, Long>{
 }
