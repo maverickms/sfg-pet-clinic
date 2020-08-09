5a3b3d71ef0d60d1739eca73cfff1b609b2b1ba5
maverickms <sharmamanven@gmail.com>
2020-08-08 23:47:52 -0700

    Added Pets to startup data loader. closes #33

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.models.Owner;
+import guru.springframework.sfgpetclinic.models.Pet;
 import guru.springframework.sfgpetclinic.models.PetType;
 import guru.springframework.sfgpetclinic.models.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -35,6 +38,15 @@ public class DataLoader implements CommandLineRunner {
         owner1.setId(1L);
         owner1.setFirstName("Rahul");
         owner1.setLastName("Dravid");
+        owner1.setAddress("121 Firenze Terrace");
+        owner1.setCity("San Jose");
+        owner1.setTelephone("213123123");
+
+        Pet rahulsPet = new Pet();
+        rahulsPet.setPetType(savedDogPetType);
+        rahulsPet.setName("Tillu");
+        rahulsPet.setBirthDate(LocalDate.now());
+        owner1.getPets().add(rahulsPet);
 
         ownerService.save(owner1);
 
@@ -42,6 +54,15 @@ public class DataLoader implements CommandLineRunner {
         owner2.setId(2L);
         owner2.setFirstName("Virendra");
         owner2.setLastName("Sehwag");
+        owner2.setAddress("333 Susa Terrace");
+        owner2.setCity("Union City");
+        owner2.setTelephone("112113114");
+
+        Pet virusPet = new Pet();
+        virusPet.setPetType(savedDogPetType);
+        virusPet.setName("Dhamaka");
+        virusPet.setBirthDate(LocalDate.now());
+        owner2.getPets().add(virusPet);
 
         ownerService.save(owner2);
 
@@ -49,6 +70,15 @@ public class DataLoader implements CommandLineRunner {
         owner3.setId(3L);
         owner3.setFirstName("Roger");
         owner3.setLastName("Federer");
+        owner3.setAddress("555 Via Lucca");
+        owner3.setCity("Newark");
+        owner3.setTelephone("2231234122");
+
+        Pet rogersPet = new Pet();
+        rogersPet.setPetType(savedCatPetType);
+        rogersPet.setName("Ace");
+        rogersPet.setBirthDate(LocalDate.now());
+        owner3.getPets().add(rogersPet);
 
         ownerService.save(owner3);
 
@@ -56,6 +86,15 @@ public class DataLoader implements CommandLineRunner {
         owner4.setId(4L);
         owner4.setFirstName("Rafael");
         owner4.setLastName("Nadal");
+        owner4.setAddress("777 North 1st Street");
+        owner4.setCity("San Jose");
+        owner4.setTelephone("4123123155");
+
+        Pet rafasPet = new Pet();
+        rafasPet.setPetType(cat);
+        rafasPet.setName("Raka");
+        rafasPet.setBirthDate(LocalDate.now());
+        owner4.getPets().add(rogersPet);
 
         ownerService.save(owner4);
 
