c7c18537935d7f295afac433852b847528ed3f68
maverickms <sharmamanven@gmail.com>
2020-08-26 16:38:55 -0700

    Converted vets to  JPA base entity, closes #38

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Vet.java
@@ -1,9 +1,16 @@
 package guru.springframework.sfgpetclinic.models;
 
+
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name =  "vets")
 public class Vet extends Person {
+    @ManyToMany(fetch = FetchType.EAGER) // with setting the fetch type to eager - JPA will load all entries at once
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
