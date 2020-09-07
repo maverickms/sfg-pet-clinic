62c3d1410dc75b4c2686b9f74d41a9828a9a31be
maverickms <sharmamanven@gmail.com>
2020-09-07 15:42:51 -0700

    Added project lombok and refactored. Also enabled for use of builder on the owner. Closes #52

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/BaseEntity.java
@@ -1,24 +1,23 @@
 package guru.springframework.sfgpetclinic.models;
 
+import lombok.*;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 // will not be saved in the database
 // used as the base class for all entities
 public class BaseEntity implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Person.java
@@ -1,30 +1,26 @@
 package guru.springframework.sfgpetclinic.models;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 // never working with this, will always be extended
 // need not be stored in the DB
 public class Person extends BaseEntity {
+
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
